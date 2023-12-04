305fd72c78c101f82e748ca76e76f274d5bfac9b
Blacktaler <Blacktaler0313@gmail.com>
2023-12-04 20:50:45 +0500

    method channel and event channel created

---
+++ lib/flutter_prevent_screen_capture_channels.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/services.dart';
+
+class FlutterPreventScreenCaptureChannels {
+  static const String _eventChannelName =
+      "blacktaler.uz/screenCaptureEvent";
+  static const String _methodChannelName =
+      "blacktaler.uz/screenCaptureMethod";
+
+  static const EventChannel eventChannel = EventChannel(_eventChannelName);
+  static const MethodChannel methodChannel = MethodChannel(_methodChannelName);
+}
